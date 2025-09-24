01dbb1f17bbf10e77bf18bdb2fa1be255ef179a4
Tran Mai Quang Khai <163870078+Khai-Tran24@users.noreply.github.com>
2025-09-24 13:56:09 +0700

    Style/import fonts (#1)
    * style: add font style Poppins and Montserrat
    
    * feat: update authentication route
    
    - change route structure
    - move login, register to folder page/authetication
    - create reset password component, forgot password component.
    
    P/S: Sorry, because my mistake commit the wrong file in the previous
    commit so i have to commit the authentication flow code in this branch.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,29 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "@/pages/Homepage";
-import Login from "@/pages/Login";
-import Register from "@/pages/Register";
+import Login from "@/pages/authentication/Login";
+import Register from "@/pages/authentication/Register";
 import PrivateRoute from "./private-route";
 import PublicRoute from "./public-route";
+import { AuthenticationRoute } from "./authentication-route";
 
 const AppRoutes = () => (
   <BrowserRouter>
     <Routes>
-      {/* Private routes: chỉ cho phép các role đã đăng nhập */}
       <Route
         element={
           <PrivateRoute
             allowedRoles={["Customer", "Admin", "Sale Staff", "Marketing Staff", "Content Staff"]}
           />
         }
-      >
-        <Route path="/" element={<Homepage />} />
-        {/* Thêm các route riêng tư khác tại đây */}
-      </Route>
+      ></Route>
 
-      {/* Public routes: chỉ cho phép người chưa đăng nhập */}
-      <Route element={<PublicRoute />}>
+      <Route element={<AuthenticationRoute />}>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<div>Forgot Password Page</div>} />
+        <Route path="/reset-password" element={<div>Reset Password Page</div>} />
+      </Route>
+
+      <Route element={<PublicRoute />}>
+        <Route path="/" element={<Homepage />} />
       </Route>
     </Routes>
   </BrowserRouter>

fe0c4aeac253fd1322684f29683e0eb50fed5a60
Kevin Truong <kevintruong6587@gmail.com>
2025-10-10 00:42:11 +0700

    fix: suggestion address error

--- src/libs/hooks/useGoong.ts
+++ src/libs/hooks/useGoong.ts
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPredictions } from "@/libs/stores/goongManager/thunk";
 import { clearPredictions } from "@/libs/stores/goongManager/slice";
@@ -7,8 +8,16 @@ export const useGoong = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { loading, predictions, error } = useSelector((state: RootState) => state.manageGoong);
 
-  const getPredictions = (input: string) => dispatch(fetchPredictions(input));
-  const clear = () => dispatch(clearPredictions());
+  const getPredictions = useCallback(
+    (input: string) => {
+      dispatch(fetchPredictions(input));
+    },
+    [dispatch],
+  );
+
+  const clear = useCallback(() => {
+    dispatch(clearPredictions());
+  }, [dispatch]);
 
   return { loading, predictions, error, getPredictions, clear };
 };

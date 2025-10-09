d5e8e887932320e74edca7887f63a5686f1e3db5
Khai_Tran <tranmaiquangkhai@gmail.com>
2025-10-09 12:43:08 +0700

    feat: call api for product

---
+++ src/libs/stores/productManager/thunk.ts
@@ -0,0 +1,29 @@
+import manageProduct from "@/libs/services/manageProduct";
+import type { ProductResponse } from "@/libs/types/product";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import type { AxiosError } from "axios";
+
+const getAllProductsThunk = createAsyncThunk("products/getAll", async (_, { rejectWithValue }) => {
+  try {
+    const response = await manageProduct.getAllProducts();
+    return response.data as ProductResponse;
+  } catch (error: AxiosError | unknown) {
+    const err = error as AxiosError<{ message: string }>;
+    return rejectWithValue(err.response?.data?.message || "Failed to fetch products");
+  }
+});
+
+const getProductByTaskIdThunk = createAsyncThunk(
+  "products/getByTaskId",
+  async (taskId: string, { rejectWithValue }) => {
+    try {
+      const response = await manageProduct.getProductByTaskId(taskId);
+      return response.data as ProductResponse;
+    } catch (error: AxiosError | unknown) {
+      const err = error as AxiosError<{ message: string }>;
+      return rejectWithValue(err.response?.data?.message || "Failed to fetch product");
+    }
+  },
+);
+
+export { getAllProductsThunk, getProductByTaskIdThunk };
--- src/pages/manager/sale/index.ts
+++ src/pages/manager/sale/index.ts
@@ -4,6 +4,6 @@ import { default as Category } from "./category/Category";
 import { default as Order } from "./order/Order";
 import { default as OrderDetail } from "./order/OrderDetail";
 import { default as Review } from "./review/Review";
-import { default as Transaction } from "./transaction/transaction";
+import { default as Transaction } from "./transaction/Transaction";
 
 export { Product, ProductDetail, Category, Order, OrderDetail, Review, Transaction };

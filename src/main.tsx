b8f0bd8ad25b16c7ae9925b6a459ad4b0465bf56
Kevin Truong <kevintruong6587@gmail.com>
2025-11-30 17:33:52 +0700

    fix: authentication for sse connection

--- src/main.tsx
+++ src/main.tsx
@@ -8,21 +8,29 @@ import { TooltipProvider } from "./components/ui/tooltip";
 import { Toaster } from "./components/ui/sonner";
 import { LoadingProvider } from "./libs/hooks/useLoading";
 import { NotificationProvider } from "@/libs/contexts/NotificationContext";
+import { useAuth } from "./libs/hooks/useAuth";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 
-root.render(
-  <React.StrictMode>
+const RootApp: React.FC = () => {
+  const { isAuthenticated } = useAuth();
+  return (
     <LoadingProvider>
-      <NotificationProvider>
-        <ReduxProvider>
-          <TooltipProvider delayDuration={200}>
-            <AppRoutes />
-          </TooltipProvider>
-          <Toaster position="top-right" richColors />
-        </ReduxProvider>
+      <NotificationProvider isAuthenticated={isAuthenticated}>
+        <TooltipProvider delayDuration={200}>
+          <AppRoutes />
+        </TooltipProvider>
+        <Toaster position="top-right" richColors />
       </NotificationProvider>
     </LoadingProvider>
+  );
+};
+
+root.render(
+  <React.StrictMode>
+    <ReduxProvider>
+      <RootApp />
+    </ReduxProvider>
   </React.StrictMode>,
 );
